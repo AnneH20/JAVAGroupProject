4fa6460e9d5b8ee05b05f8ffe36bc214ba61f1e7
EvieSimpson <ems735@msstate.edu>
2023-11-25 18:46:00 -0600

    Update Employee.java

--- ManagementProgramGroupProject/JAVAGroupProject/src/javaGUI/Employee.java
+++ ManagementProgramGroupProject/JAVAGroupProject/src/javaGUI/Employee.java
@@ -4,8 +4,11 @@ import java.awt.*;
 import java.awt.event.*;
 
 class Employee extends JFrame {
-    JLabel l1;
+    JLabel l1, clockLabel;
     JButton b1, b2, b3, b4;
+    long startTime, endTime;
+    boolean clockedIn; //Flag to track if clocked in
+    
 
     Employee() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -15,6 +18,11 @@ class Employee extends JFrame {
         l1.setForeground(Color.BLUE);
         l1.setBounds(100, 10, 300, 30);
         add(l1);
+        
+        clockLabel = new JLabel("00:00:00");
+        clockLabel.setBounds(150,60,100,30);
+        clockLabel.setVisible(false);
+        add(clockLabel);
 
         b1 = new JButton("Clock In");
         b2 = new JButton("Clock Out");
@@ -27,6 +35,23 @@ class Employee extends JFrame {
         add(b1);
         add(b2);
         add(b3);
+        
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                startTime = System.currentTimeMillis();
+                clockedIn = true; //Set clockedIn flag to true
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+            	if (clockedIn) {
+                endTime = System.currentTimeMillis();
+                updateClock(); // Update clock label only if clocked in
+                clockedIn = false; //Reset clockedIn flag
+              }
+            }
+        });
 
         b3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
@@ -37,7 +62,24 @@ class Employee extends JFrame {
                 loginScreen.setVisible(true); // Show the login screen
             }
         });
+        
+        //Initially hide the clock label
+        clockLabel.setVisible(false);
     }
+    
+    // Method to update the clock label with elapsed time
+    private void updateClock() {
+        long elapsed = endTime - startTime;
+        int hours = (int) (elapsed / 3600000);
+        int minutes = (int) ((elapsed % 3600000) / 60000);
+        int seconds = (int) ((elapsed % 60000) / 1000);
+
+        String time = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        clockLabel.setText(time);
+        clockLabel.setVisible(true);
+    }
+    
+    
     Login loginScreen;
     Employee(Login loginScreen){
     	this.loginScreen = loginScreen;
